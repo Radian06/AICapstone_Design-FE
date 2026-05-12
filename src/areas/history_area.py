042f9aea13ab4f4721e4935adec5223a43d3cc9d
Radian06 <aksen05aksen@gmail.com>
2026-05-12 23:43:15 +0900

    feat(#6): 기록 구역 제목 추가

--- src/areas/history_area.py
+++ src/areas/history_area.py
@@ -6,15 +6,16 @@ class HistoryArea(QFrame):
         super().__init__()
         
         # 기록 구역 기본 스타일 및 크기 설정
-        self.setStyleSheet("background-color: #4A4A4A;")
+        self.setStyleSheet("background-color: #25343F;")
+        self.setContentsMargins(20, 60, 20, 60)
         self.setFixedWidth(250) # 너비 고정
         
         # 레이아웃 생성
         layout = QVBoxLayout(self)
         
         # 제목 라벨
-        title_label = QLabel("과거 질문 내역")
-        title_label.setStyleSheet("color: white; font-weight: bold;")
+        title_label = QLabel("쉽게 알아보는\n소송 걸기")
+        title_label.setStyleSheet("color: #EAEFEF; font-weight: bold; font-size: 20px;")
         title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
         layout.addWidget(title_label)
